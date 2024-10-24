008ba25efb69e8fd0f4a64d078b79fe9db5afbeb
aminammar1 <ammar.mohamdamine@gmail.com>
2024-10-24 11:57:23 +0100

    v1.1

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app = express()
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.listen(3000 , () => {
-    console.log('server is runing on port 3000 ')
-}
-); 
+const app = express();
+
+app.listen(3000, () => {
+  console.log("server is runing on port 3000 ");
+});
