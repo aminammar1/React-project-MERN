4b8f86e1c048c66ea2a5499d5548e4e6eefda28b
aminammar1 <ammar.mohamdamine@gmail.com>
2024-11-21 17:40:37 +0100

    Complete Listing functionality integration

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,11 +1,29 @@
 import { useState } from "react";
 import { Client, Storage } from "appwrite";
+import { useSelector } from "react-redux";
 
 export default function CreateListing() {
   const [files, setFiles] = useState([]);
   const [uploading, setUploading] = useState(false);
-  const [formData, setFormData] = useState({ imageUrls: [] });
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+    name: "",
+    description: "",
+    address: "",
+    type: "rent",
+    bedrooms: 1,
+    bathrooms: 1,
+    regularPrice: 50,
+    discountedPrice: 50,
+    offer: false,
+    parking: false,
+    furnished: false,
+  });
+  console.log(formData);
   const [imageUploadError, setImageUploadError] = useState(false);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const { currentUser } = useSelector((state) => state.user);
 
   const client = new Client();
   client
@@ -79,12 +97,66 @@ export default function CreateListing() {
       imageUrls: formData.imageUrls.filter((_, i) => i !== index),
     }));
   };
+
+  const handleChange = (e) => {
+    if (e.target.id === "sale" || e.target.id === "rent") {
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        type: e.target.id,
+      }));
+    }
+    if (
+      e.target.id === "parking" ||
+      e.target.id === "furnished" ||
+      e.target.id === "offer"
+    ) {
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        [e.target.id]: e.target.checked,
+      }));
+    }
+    if (
+      e.target.type === "number" ||
+      e.target.type === "text" ||
+      e.target.type === "textarea"
+    ) {
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        [e.target.id]: e.target.value,
+      }));
+    }
+  };
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/listing/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...formData,
+          userRef: currentUser._id,
+        }),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(data.message);
+      }
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
         Create Listing
       </h1>
-      <form className="flex flex-col sm:flex-row gap-4">
+      <form onSubmit={handelSubmit} className="flex flex-col sm:flex-row gap-4">
         <div className="flex flex-col gap-4 flex-1">
           <input
             type="text"
@@ -94,6 +166,8 @@ export default function CreateListing() {
             maxLength="50"
             minLength="10"
             required
+            onChange={handleChange}
+            value={formData.name}
           />
           <input
             type="text"
@@ -101,6 +175,8 @@ export default function CreateListing() {
             className="border p-3 rounded-lg"
             id="description"
             required
+            onChange={handleChange}
+            value={formData.description}
           />
           <input
             type="text"
@@ -108,26 +184,58 @@ export default function CreateListing() {
             className="border p-3 rounded-lg"
             id="address"
             required
+            onChange={handleChange}
+            value={formData.address}
           />
           <div className="flex gap-6 flex-wrap">
             <div className="flex gap-2">
-              <input type="checkbox" id="sale" className="w-5" />
+              <input
+                type="checkbox"
+                id="sale"
+                className="w-5"
+                onChange={handleChange}
+                checked={formData.type == "sale"}
+              />
               <span>Sell</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="rent" className="w-5" />
+              <input
+                type="checkbox"
+                id="rent"
+                className="w-5"
+                onChange={handleChange}
+                checked={formData.type == "rent"}
+              />
               <span>Rent</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="parking" className="w-5" />
+              <input
+                type="checkbox"
+                id="parking"
+                className="w-5"
+                onChange={handleChange}
+                checked={formData.parking}
+              />
               <span>Parking</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="furnished" className="w-5" />
+              <input
+                type="checkbox"
+                id="furnished"
+                className="w-5"
+                onChange={handleChange}
+                checked={formData.furnished}
+              />
               <span>Furnished</span>
             </div>
             <div className="flex gap-2">
-              <input type="checkbox" id="offer" className="w-5" />
+              <input
+                type="checkbox"
+                id="offer"
+                className="w-5"
+                onChange={handleChange}
+                checked={formData.offer}
+              />
               <span>Offer</span>
             </div>
           </div>
@@ -140,6 +248,8 @@ export default function CreateListing() {
                 max="10"
                 required
                 className="border border-gray-300 p-3 rounded-lg"
+                onChange={handleChange}
+                checked={formData.bedrooms}
               />
               <p>Beds</p>
             </div>
@@ -151,6 +261,8 @@ export default function CreateListing() {
                 max="10"
                 required
                 className="border border-gray-300 p-3 rounded-lg"
+                onChange={handleChange}
+                checked={formData.bathrooms}
               />
               <p>Baths</p>
             </div>
@@ -158,9 +270,12 @@ export default function CreateListing() {
               <input
                 type="number"
                 id="regularPrice"
-                min="1"
+                min="50"
+                max="1000000"
                 required
                 className="border border-gray-300 p-3 rounded-lg"
+                onChange={handleChange}
+                checked={formData.regularPrice}
               />
               <div className="flex flex-col items-center">
                 <p>Regular Price</p>
@@ -171,8 +286,11 @@ export default function CreateListing() {
               <input
                 type="number"
                 id="discountedPrice"
-                min="1"
+                min="50"
+                max="1000000"
                 className="border border-gray-300 p-3 rounded-lg"
+                onChange={handleChange}
+                checked={formData.discountedPrice}
               />
               <div className="flex flex-col items-center">
                 <p>Discounted Price</p>
@@ -232,8 +350,9 @@ export default function CreateListing() {
               </div>
             ))}
           <button className="p-3 bg-green-500 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-            Create Listing
+            {loading ? "Loading..." : "Create Listing"}
           </button>
+          {error && <p className="text-red-600">{error}</p>}
         </div>
       </form>
     </main>
--- server/models/listing.model.js
+++ server/models/listing.model.js
@@ -10,7 +10,7 @@ const listingSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    adress: {
+    address: {
       type: String,
       required: true,
     },
@@ -18,7 +18,7 @@ const listingSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
-    discountPrice: {
+    discountedPrice: {
       type: Number,
       required: true,
     },
