d388992a9be94662bf8b27daa8abcfce53fb2d90
aminammar1 <ammar.mohamdamine@gmail.com>
2024-11-21 16:57:19 +0100

    Create Listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,9 +1,88 @@
+import { useState } from "react";
+import { Client, Storage } from "appwrite";
+
 export default function CreateListing() {
+  const [files, setFiles] = useState([]);
+  const [uploading, setUploading] = useState(false);
+  const [formData, setFormData] = useState({ imageUrls: [] });
+  const [imageUploadError, setImageUploadError] = useState(false);
+
+  const client = new Client();
+  client
+    .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+    .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+
+  const storage = new Storage(client);
+
+  const handleImageSubmit = async () => {
+    if (files.length > 0 && files.length + formData.imageUrls.length <= 6) {
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        if (file.size > 2 * 1024 * 1024) {
+          setImageUploadError("Image size should be less than 2MB.");
+          setUploading(false);
+          return;
+        }
+      }
+
+      setUploading(true);
+      setImageUploadError(false);
+      const promises = Array.from(files).map((file, index) =>
+        storeImage(file, index)
+      );
+
+      try {
+        const urls = await Promise.all(promises);
+        setFormData((formData) => ({
+          ...formData,
+          imageUrls: [...formData.imageUrls, ...urls],
+        }));
+        setImageUploadError(false);
+        setUploading(false);
+      } catch {
+        setImageUploadError("Image upload failed. Please try again.");
+      }
+    } else {
+      setImageUploadError("You can only upload a maximum of 6 images.");
+      setUploading(false);
+    }
+  };
+
+  const storeImage = (file) => {
+    return new Promise((resolve, reject) => {
+      const uploadFile = async () => {
+        try {
+          const response = await storage.createFile(
+            import.meta.env.VITE_APPWRITE_BUCKET_ID,
+            "unique()",
+            file
+          );
+
+          const fileUrl = `${
+            import.meta.env.VITE_APPWRITE_ENDPOINT
+          }/storage/buckets/${import.meta.env.VITE_APPWRITE_BUCKET_ID}/files/${
+            response.$id
+          }/view?project=${import.meta.env.VITE_APPWRITE_PROJECT_ID}`;
+
+          resolve(fileUrl);
+        } catch (error) {
+          reject(error);
+        }
+      };
+      uploadFile();
+    });
+  };
+
+  const handelRemoveImage = (index) => {
+    setFormData((formData) => ({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+    }));
+  };
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
-        {" "}
-        Create Listing{" "}
+        Create Listing
       </h1>
       <form className="flex flex-col sm:flex-row gap-4">
         <div className="flex flex-col gap-4 flex-1">
@@ -31,28 +110,28 @@ export default function CreateListing() {
             required
           />
           <div className="flex gap-6 flex-wrap">
-            <div className="flex gap-2 ">
+            <div className="flex gap-2">
               <input type="checkbox" id="sale" className="w-5" />
-              <span> Sell </span>
+              <span>Sell</span>
             </div>
-            <div className="flex gap-2 ">
+            <div className="flex gap-2">
               <input type="checkbox" id="rent" className="w-5" />
-              <span> Rent </span>
+              <span>Rent</span>
             </div>
-            <div className="flex gap-2 ">
+            <div className="flex gap-2">
               <input type="checkbox" id="parking" className="w-5" />
-              <span> Parking </span>
+              <span>Parking</span>
             </div>
-            <div className="flex gap-2 ">
+            <div className="flex gap-2">
               <input type="checkbox" id="furnished" className="w-5" />
-              <span> Furnished </span>
+              <span>Furnished</span>
             </div>
-            <div className="flex gap-2 ">
+            <div className="flex gap-2">
               <input type="checkbox" id="offer" className="w-5" />
-              <span> Offer </span>
+              <span>Offer</span>
             </div>
           </div>
-          <div className=" flex flex-wrap gap-6">
+          <div className="flex flex-wrap gap-6">
             <div className="flex items-center gap-2">
               <input
                 type="number"
@@ -80,13 +159,12 @@ export default function CreateListing() {
                 type="number"
                 id="regularPrice"
                 min="1"
-                max="10"
                 required
                 className="border border-gray-300 p-3 rounded-lg"
               />
-              <div className=" flex flex-col items-center">
-                <p>Regular Price </p>
-                <span className="text-xs"> ($ / month) </span>
+              <div className="flex flex-col items-center">
+                <p>Regular Price</p>
+                <span className="text-xs">($ / month)</span>
               </div>
             </div>
             <div className="flex items-center gap-2">
@@ -94,42 +172,67 @@ export default function CreateListing() {
                 type="number"
                 id="discountedPrice"
                 min="1"
-                max="10"
-                required
                 className="border border-gray-300 p-3 rounded-lg"
               />
-              <div className=" flex flex-col items-center">
-                <p> Discounted Price </p>
-                <span className="text-xs"> ($ / month) </span>
+              <div className="flex flex-col items-center">
+                <p>Discounted Price</p>
+                <span className="text-xs">($ / month)</span>
               </div>
             </div>
           </div>
         </div>
         <div className="flex flex-col flex-1 gap-4">
           <p className="font-semibold">
-            {" "}
             Images:
             <span className="font-normal text-gray-600 ml-2">
-              {" "}
-              the first image will be the cover (max 6){" "}
+              the first image will be the cover (max 6)
             </span>
           </p>
-          <div className=" flex gap-4">
+          <div className="flex gap-4">
             <input
+              onChange={(e) => setFiles(e.target.files)}
               className="border border-gray-300 p-3 rounded w-full"
               type="file"
               id="images"
               accept="image/*"
               multiple
             />
-            <button className="p-3 text-green-600 border border-green-600 rounded uppercase hover:shadow-lg disabled:opacity-80">
-              {" "}
-              Upload{" "}
+            <button
+              type="button"
+              disabled={uploading}
+              onClick={handleImageSubmit}
+              className="p-3 text-green-600 border border-green-600 rounded uppercase hover:shadow-lg disabled:opacity-80"
+            >
+              {uploading ? "Uploading..." : "Upload"}
             </button>
           </div>
-          <button className="p-3 bg-green-500 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80 ">
+          <p className="text-red-600">
             {" "}
-            Create Listing{" "}
+            {imageUploadError && imageUploadError}{" "}
+          </p>
+          {formData.imageUrls.length > 0 &&
+            formData.imageUrls.map((url, index) => (
+              <div
+                key={url}
+                className="flex justify-between p-3 border items-center"
+              >
+                <img
+                  src={url}
+                  alt="listing image"
+                  className="w-20 h-20 object-contain rounded-lg"
+                />
+                <button
+                  type="button"
+                  onClick={() => handelRemoveImage(index)}
+                  className="p-3 text-red-700 rounded-lg uppercase hover:opacity-95 "
+                >
+                  {" "}
+                  Delete
+                </button>
+              </div>
+            ))}
+          <button className="p-3 bg-green-500 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+            Create Listing
           </button>
         </div>
       </form>
--- server/models/listing.model.js
+++ server/models/listing.model.js
@@ -46,7 +46,7 @@ const listingSchema = new mongoose.Schema(
       type: Boolean,
       required: true,
     },
-    images: {
+    imageUrls: {
       type: Array,
       required: true,
     },
